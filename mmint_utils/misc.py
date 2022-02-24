1c54f5619427679aa781da66b3aac1ed5515d732
Mark Van der Merwe <markvdm@umich.edu>
2022-02-24 14:11:00 -0500

    Load pickle.

--- mmint_utils/misc.py
+++ mmint_utils/misc.py
@@ -20,6 +20,15 @@ def load_gzip_pickle(filename):
     return data
 
 
+def load_pickle(filename):
+    """
+    Load from file using pickle.
+    """
+    with open(filename, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+
 def make_dir(directory):
     """
     If the provided directory does not exist, make it.
