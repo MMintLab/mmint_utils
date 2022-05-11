49d2b277fbff20c14aa71deab2861a8f7a3d7725
Mark Van der Merwe <markvdm@umich.edu>
2022-05-11 16:45:14 -0400

    Fix imports.

--- mmint_utils/__init__.py
+++ mmint_utils/__init__.py
@@ -2,7 +2,7 @@ from mmint_utils.config import (
     load_cfg, dump_cfg, combine_cfg,
 )
 from mmint_utils.misc import (
-    load_gzip_pickle, save_gzip_pickle, make_dir, load_pickle
+    load_gzip_pickle, save_gzip_pickle, make_dir, load_pickle, generate_splits_indices
 )
 
 import mmint_utils.terminal_colors
