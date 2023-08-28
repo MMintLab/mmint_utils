8398954136a575526c0d3286383f465c2a3f1d8e
EClymk <eclymk@gmail.com>
2023-08-28 11:28:47 -0400

    ongoing work on gravity compensation

---
+++ tests/gravity_compensation/gravity_compensation.py
@@ -0,0 +1,4 @@
+# This scripts is to compensate the netft measurement on hand with the gravity compensation, read parameters from file
+
+import rospy
+import numpy as np
--- tests/gravity_compensation/gravity_parameters_acquisition.py
+++ tests/gravity_compensation/gravity_parameters_acquisition.py
@@ -22,6 +22,7 @@ fixed_position_down = np.array([0.5, 0, 0.3])
 fixed_position_up = np.array([0.5, 0, 0.75])
 fixed_position_horizontal = np.array([0.7, 0, 0.5])
 
+gravity_acc = 9.81
 
 def wrench_record_callback(data):
     pass
@@ -41,21 +42,34 @@ def process_gravity_parameters(wrench_up, wrench_left, wrench_right):
     '''
     This function is to process the gravity parameters, and return the gravity parameters 
     which are mass, center of mass in gamma frame, now ignore the inertia  
+    'left', 'right' are just labels for different wrench
     '''
-    gravity_acc = 9.81
-    mass_initial_measurement = wrench_up[2]/gravity_acc # maybe need to invert the sign
+    
+    equivalent_wrench = - 0.5 * wrench_up # this is the equivalent wrench applied to gamma wehn down pose caused by gravity, only to de-zero gamma
+    
+    mass, center_x, center_y = wrench_to_mass_center(equivalent_wrench)
+    center = [center_x, center_y]
+    return equivalent_wrench, mass, center
+                            
+def wrench_to_mass_center(wrench):
+    # Assuming vertical, recover mass and dist for compensation
+    mass = wrench[2] / gravity_acc
+    # also assuming force is only along Z axis, then center_x and center_y are:
+    center_x = - wrench[4] / wrench[2]
+    center_y = wrench[3] / wrench[2]        # TODO: check signs
+    center = np.cross()
+    return mass, center
     
     
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-    pass    
+    
 
 if __name__ == '__main__':
     '''
     This script is to set the gamma sensor to different posese to acquire the gravity parameters
     1. set the gamma sensor to vertical, down pose, and zero the gamma sensor
     2. set the gamma sensor to vertical, up pose, and get the measurement, as 2*mg
-    3. set the gamma sensor to horizontal, left pose, both to validate mg, and also using the torque to get the center of mass in one direction
-    4. set the gamma sensor to horizontal, right pose, both to validate mg, and also using the torque to get the center of mass in another direction
+    #3. set the gamma sensor to horizontal, left pose, both to validate mg, and also using the torque to get the center of mass in one direction
+    #4. set the gamma sensor to horizontal, right pose, both to validate mg, and also using the torque to get the center of mass in another direction
     Then save the parameters, for the gravity compensation
     '''
     # pass
@@ -75,7 +89,7 @@ if __name__ == '__main__':
     med.set_raw_pose(np.concatenate([fixed_position_horizontal, fixed_orientation_quat_horizontal_left]), blocking=True)
     rospy.sleep(2)
     left_wrench_data = wrench_record_filter(filter_length=10)
-    # set the gamma sensor to horizontal, right pose, both to validate mg, and also using the torque to get the center of mass in another direction
+    # # set the gamma sensor to horizontal, right pose, both to validate mg, and also using the torque to get the center of mass in another direction
     med.set_raw_pose(np.concatenate([fixed_position_horizontal, fixed_orientation_quat_horizontal_right]), blocking=True)
     rospy.sleep(2)
     right_wrench_data = wrench_record_filter(filter_length=10)
