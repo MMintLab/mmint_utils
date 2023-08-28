5ebafc04c1b0a44b1aeb64d2708671632d920fbc
EClymk <eclymk@gmail.com>
2023-08-28 16:58:27 -0400

    half finished, should now test and debug

--- tests/gravity_compensation/gravity_compensation.py
+++ tests/gravity_compensation/gravity_compensation.py
@@ -1,4 +1,72 @@
+#! /usr/bin/env python
 # This scripts is to compensate the netft measurement on hand with the gravity compensation, read parameters from file
 
 import rospy
 import numpy as np
+import argparse
+
+from geometry_msgs.msg import WrenchStamped, PoseStamped
+from mmint_utils.config import dump_cfg, load_cfg
+from tf import TransformListener
+import tf.transformations as tr
+import copy
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser('Gamma_on_hand_compensation')
+    # parser.add_argument('--gamma_on_hand', default=True, action='store_true')
+    # args = parser.parse_args()
+    # if args.gamma_on_hand:
+    rospy.init_node("netft_compensated")
+    gravity_cfg = load_cfg('config/gravity_params.yaml')
+    equivalent_wrench = gravity_cfg['equivanlent_wrench']
+    mass = gravity_cfg['mass']
+    center = gravity_cfg['center']
+    gravity_acc = 9.81
+    gravity_vector  = np.array([0, 0, - mass * gravity_acc])
+    center_in_gamma_frame = np.array(center)
+    
+    # compensated_wrench pub:
+    compensated_wrench_pub = rospy.Publisher('/netft/compensated_netft_data', WrenchStamped)
+    # tf for C.o.M, with fixed orientation in world frame
+    tf_listener = TransformListener()
+    world_frame_name = 'world'
+    gamma_frame_name = 'gamma_on_hand_link_ft'
+    
+    def wrench_callback(data):
+        '''
+            feed in wrench from netft, and then publish the compensated wrench
+        '''
+        
+        tf_listener.waitForTransform(world_frame_name, gamma_frame_name, rospy.Time(), rospy.Duration(1.0))
+        t = tf_listener.getLatestCommonTime(world_frame_name, gamma_frame_name)
+        world_to_gamma_frame_transform = tf_listener.lookupTransform(world_frame_name, gamma_frame_name, t)
+        world_to_gamma_quaternion = np.array(world_to_gamma_frame_transform[1])
+        world_to_gamma_rotation_matrix = tr.quaternion_matrix(world_to_gamma_quaternion)
+        force_in_gamma_frame = world_to_gamma_rotation_matrix @ gravity_vector
+        torque_in_gamma_frame = np.cross(center_in_gamma_frame , force_in_gamma_frame)
+        # only for debugging: 
+        print('force:', force_in_gamma_frame)
+        print('torque:', torque_in_gamma_frame)
+        
+        new_wrenchstamped = copy.deepcopy(data)
+        new_wrenchstamped.wrench.force.x = data.wrench.force.x - equivalent_wrench[0] - force_in_gamma_frame[0]
+        new_wrenchstamped.wrench.force.y = data.wrench.force.y - equivalent_wrench[1] - force_in_gamma_frame[1]
+        new_wrenchstamped.wrench.force.z = data.wrench.force.z - equivalent_wrench[2] - force_in_gamma_frame[2]
+        
+        new_wrenchstamped.wrench.torque.x = data.wrench.torque.x - equivalent_wrench[3] - torque_in_gamma_frame[0]
+        new_wrenchstamped.wrench.torque.y = data.wrench.torque.y - equivalent_wrench[4] - torque_in_gamma_frame[1]
+        new_wrenchstamped.wrench.torque.z = data.wrench.torque.z - equivalent_wrench[5] - torque_in_gamma_frame[2]
+                
+        compensated_wrench_pub.publish(new_wrenchstamped)
+        return 
+    
+    # netft_sub:
+    netft_sub = rospy.Subscriber('/netft/netft_data', WrenchStamped, wrench_callback)
+    # get the current com frame in gamma's frame
+    rospy.spin()
+
+    # transformation from gamma's center to com is center
+    # gravity direction in gamma's frame is 
+    
+        
+        
--- tests/gravity_compensation/gravity_parameters_acquisition.py
+++ tests/gravity_compensation/gravity_parameters_acquisition.py
@@ -11,16 +11,17 @@ from bubble_utils.bubble_med.bubble_med import BubbleMed
 from mmint_utils.gamma_helpers import zero_ati_gamma
 
 from mmint_tools import tr
+from mmint_utils.config import dump_cfg, load_cfg
 
 
 fixed_orientation_quat_down = tr.quaternion_from_euler(0, np.pi, np.pi*0.5, axes='sxyz')     # this is the down orientation
-fixed_orientation_quat_up = tr.quaternion_from_euler(0, np.pi, np.pi*1.5, axes='sxyz')  # this is the up orientation
-fixed_orientation_quat_horizontal_left = tr.quaternion_from_euler(0, np.pi, np.pi, axes='sxyz')  # this is the horizontal left orientation
-fixed_orientation_quat_horizontal_right = tr.quaternion_from_euler(0, np.pi, 0, axes='sxyz')  # this is the horizontal right orientation
+fixed_orientation_quat_up = tr.quaternion_from_euler(np.pi, np.pi, np.pi*0.5, axes='sxyz')  # this is the up orientation
+fixed_orientation_quat_horizontal_left = tr.quaternion_from_euler(np.pi*0.5, np.pi, np.pi*0.5, axes='sxyz')  # this is the horizontal left orientation
+fixed_orientation_quat_horizontal_right = tr.quaternion_from_euler(np.pi*0.5, np.pi*0.5, np.pi*0.5, axes='sxyz')  # this is the horizontal right orientation
 
-fixed_position_down = np.array([0.5, 0, 0.3])
-fixed_position_up = np.array([0.5, 0, 0.75])
-fixed_position_horizontal = np.array([0.7, 0, 0.5])
+fixed_position_down = np.array([0.6, 0, 0.4])
+fixed_position_up = np.array([0.6, 0, 1.2])
+fixed_position_horizontal = np.array([0.8, 0, 0.8])
 
 gravity_acc = 9.81
 
@@ -48,8 +49,12 @@ def process_gravity_parameters(wrench_up, wrench_left, wrench_right):
     equivalent_wrench = - 0.5 * wrench_up # this is the equivalent wrench applied to gamma wehn down pose caused by gravity, only to de-zero gamma
     
     mass, center_x, center_y = wrench_to_mass_center(equivalent_wrench)
-    center = [center_x, center_y]
-    return equivalent_wrench, mass, center
+    center_z_left = wrench_left[4] / wrench_left[2]
+    center_z_right = wrench_right[3] / wrench_right[2]
+    center_z = (center_z_left + center_z_right) / 2
+    center = [center_x, center_y, center_z]
+    # import pdb; pdb.set_trace()
+    return equivalent_wrench.tolist(), float(mass), center.tolist()
                             
 def wrench_to_mass_center(wrench):
     # Assuming vertical, recover mass and dist for compensation
@@ -57,10 +62,20 @@ def wrench_to_mass_center(wrench):
     # also assuming force is only along Z axis, then center_x and center_y are:
     center_x = - wrench[4] / wrench[2]
     center_y = wrench[3] / wrench[2]        # TODO: check signs
-    center = np.cross()
-    return mass, center
+    return mass, center_x, center_y
     
-    
+
+def store_gravity_params(equivalent_wrench, mass, center, dir_conf=f'config/gravity_params.yaml'):
+    '''
+        store the calculated parameters to config file
+    '''
+    gravity_cfg = {
+            "equivanlent_wrench": equivalent_wrench,
+            "mass": mass,
+            "center": center
+        }
+    dump_cfg(dir_conf, gravity_cfg)
+    return
     
 
 if __name__ == '__main__':
@@ -81,24 +96,30 @@ if __name__ == '__main__':
     rospy.sleep(2)
     zero_ati_gamma()
     rospy.sleep(2)
+    
     # set the gamma sensor to vertical, up pose, and get the measurement, as 2*mg
     med.set_raw_pose(np.concatenate([fixed_position_up, fixed_orientation_quat_up]), blocking=True)
     rospy.sleep(2)
     up_wrench_data = wrench_record_filter(filter_length=10)
+    
     # set the gamma sensor to horizontal, left pose, both to validate mg, and also using the torque to get the center of mass in one direction
     med.set_raw_pose(np.concatenate([fixed_position_horizontal, fixed_orientation_quat_horizontal_left]), blocking=True)
     rospy.sleep(2)
     left_wrench_data = wrench_record_filter(filter_length=10)
+    
     # # set the gamma sensor to horizontal, right pose, both to validate mg, and also using the torque to get the center of mass in another direction
     med.set_raw_pose(np.concatenate([fixed_position_horizontal, fixed_orientation_quat_horizontal_right]), blocking=True)
     rospy.sleep(2)
     right_wrench_data = wrench_record_filter(filter_length=10)
     
-    gravity_params = process_gravity_parameters(up_wrench_data, left_wrench_data, right_wrench_data)
-    
+    equivalent_wrench, mass, center = process_gravity_parameters(up_wrench_data, left_wrench_data, right_wrench_data)    
+    # first to list:
     ## store the gravity parameters
     
-    
+    # equivalent_wrench = [0,0,0,0,0,0]
+    # mass = 2
+    # center = [1,1,1]
+    store_gravity_params(equivalent_wrench, mass, center)
     
     
     
