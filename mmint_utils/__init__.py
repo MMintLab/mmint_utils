b04dd2e3cfa799e1e903cc3468d9459142e681cb
Mark Van der Merwe <markvdm@umich.edu>
2025-07-27 20:29:28 -0400

    Add save_pickle to init

--- mmint_utils/__init__.py
+++ mmint_utils/__init__.py
@@ -2,7 +2,7 @@ from mmint_utils.config import (
     load_cfg, dump_cfg, combine_dict,
 )
 from mmint_utils.misc import (
-    load_gzip_pickle, save_gzip_pickle, make_dir, load_pickle, generate_splits_indices
+    load_gzip_pickle, save_gzip_pickle, make_dir, load_pickle, generate_splits_indices, save_pickle
 )
 
 import mmint_utils.terminal_colors
